0497f140218be4e78ef07091980460fc2e00d7ae
unknown <giuliazeolla23@gmail.com>
2025-07-08 21:49:07 +0200

    fix models

--- backend/models/Mezzo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const mezzoSchema = new mongoose.Schema({
-  nome: { type: String, required: true, unique: true },
-  attivita: { type: mongoose.Schema.Types.ObjectId, ref: 'Attivita' }
-});
-
-module.exports = mongoose.model('Mezzo', mezzoSchema);
--- backend/models/Operaio.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const operaioSchema = new mongoose.Schema({
-  nome: { type: String, required: true, unique: true },
-  attivita: { type: mongoose.Schema.Types.ObjectId, ref: 'Attivita' }
-});
-
-module.exports = mongoose.model('Operaio', operaioSchema);
